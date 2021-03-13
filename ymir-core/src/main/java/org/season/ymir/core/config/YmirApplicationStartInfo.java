8e7d402f35cec647b99691e4c785e63366c51ae1
KevinClair <704714211@qq.com>
2021-03-13 19:36:14 +0800

    Add core module.

---
+++ ymir-core/src/main/java/org/season/ymir/core/config/YmirApplicationStartInfo.java
@@ -0,0 +1,18 @@
+package org.season.ymir.core.config;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+
+/**
+ * TODO
+ *
+ * @author KevinClair
+ */
+public class YmirApplicationStartInfo implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+
+    }
+
+}
