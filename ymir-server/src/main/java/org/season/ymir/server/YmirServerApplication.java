da9baaaececbd57e2c9cce3cebc2ab5ebbdad386
KevinClair <704714211@qq.com>
2021-03-13 19:35:58 +0800

    Add server application.

---
+++ ymir-server/src/main/java/org/season/ymir/server/YmirServerApplication.java
@@ -0,0 +1,18 @@
+package org.season.ymir.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 服务端启动类
+ *
+ * @author KevinClair
+ */
+@SpringBootApplication
+public class YmirServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmirServerApplication.class, args);
+    }
+
+}
