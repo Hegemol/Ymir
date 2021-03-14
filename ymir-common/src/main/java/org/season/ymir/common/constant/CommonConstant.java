f8d97692070dbdb9322e8fc51f82656af29f2094
KevinClair <704714211@qq.com>
2021-03-14 18:48:21 +0800

    添加Constant；

---
+++ ymir-common/src/main/java/org/season/ymir/common/constant/CommonConstant.java
@@ -0,0 +1,34 @@
+package org.season.ymir.common.constant;
+
+/**
+ * TODO
+ *
+ * @author KevinClair
+ */
+public class CommonConstant {
+
+    /***
+     * 编码
+     */
+    public static final String UTF_8 = "UTF-8";
+
+    /**
+     * Zookeeper服务注册地址
+     */
+    public static final String ZK_SERVICE_PATH = "/ymir";
+
+    /**
+     * Zookeeper客户端地址
+     */
+    public static final String ZK_SERVICE_CLIENT_PATH = "/client";
+
+    /**
+     * Zookeeper服务端地址
+     */
+    public static final String ZK_SERVICE_SERVER_PATH = "/server";
+
+    /**
+     * 路径分隔符
+     */
+    public static final String PATH_DELIMITER = "/";
+}
