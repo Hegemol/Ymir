77c73b97b1525ecefaa5c8f21db47e58450f497c
KevinClair <704714211@qq.com>
2021-03-20 17:38:12 +0800

    Add MessageProtocol.

--- src/main/java/org/season/ymir/core/handler/MessageProtocol.java
+++ src/main/java/org/season/ymir/core/handler/MessageProtocol.java
@@ -1,9 +1,42 @@
 package org.season.ymir.core.handler;
 
+import org.season.ymir.common.model.YmirRequest;
+import org.season.ymir.common.model.YmirResponse;
+
 /**
  * 消息协议
  *
  * @author KevinClair
  */
-public class MessageProtocol {
+public interface MessageProtocol {
+    /**
+     * 编组请求
+     * @param request 请求信息
+     * @return 请求字节数组
+     * @throws Exception
+     */
+    byte[] marshallingRequest(YmirRequest request) throws Exception;
+
+    /**
+     * 解组请求
+     * @param data
+     * @return
+     * @throws Exception
+     */
+    YmirRequest unmarshallingRequest(byte[] data) throws Exception;
+
+    /**
+     * 编组响应
+     * @param response
+     * @return
+     */
+    byte[] marshallingResponse(YmirResponse response) throws Exception;
+
+    /**
+     * 解组响应
+     * @param data
+     * @return
+     * @throws Exception
+     */
+    YmirResponse unmarshallingResponse(byte[] data) throws Exception;
 }
