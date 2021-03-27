6b15ce6242947ebd938078d201e545817cfc626a
KevinClair <704714211@qq.com>
2021-03-27 23:40:18 +0800

    注册zk服务注册中心Bean；

--- src/main/java/org/season/ymir/client/configuration/YmirClientAutoConfiguration.java
+++ src/main/java/org/season/ymir/client/configuration/YmirClientAutoConfiguration.java
@@ -1,9 +1,19 @@
 package org.season.ymir.client.configuration;
 
+import org.season.ymir.client.register.ZookeeperServiceRegister;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
 /**
  * 客户端Bean注入
  *
  * @author KevinClair
  */
+@EnableConfigurationProperties(YmirClientPropertySource.class)
 public class YmirClientAutoConfiguration {
+
+    @Bean
+    public ZookeeperServiceRegister zookeeperServiceRegister(YmirClientPropertySource propertySource){
+        return new ZookeeperServiceRegister(propertySource.getAddress(), 20777, propertySource.getPort(), 100);
+    }
 }
