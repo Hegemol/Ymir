dca17610cd3822b9607de486fb0c2e2ef548d576
KevinClair <704714211@qq.com>
2021-03-28 13:33:28 +0800

    添加Zookeeper路径工具类；

---
+++ src/main/java/org/season/ymir/common/utils/ZkPathUtils.java
@@ -0,0 +1,26 @@
+package org.season.ymir.common.utils;
+
+import org.season.ymir.common.constant.CommonConstant;
+
+/**
+ * zookeeper的node节点路径
+ *
+ * @author KevinClair
+ */
+public class ZkPathUtils {
+
+    /**
+     * 生成zk节点路径
+     *
+     * @param side provider or consumer
+     * @param name 接口名
+     * @return
+     */
+    public static String buildPath(String side, String name){
+        return String.join(CommonConstant.PATH_DELIMITER, CommonConstant.ZK_SERVICE_PATH, name, side);
+    }
+
+    public static String buildUriPath(String nodePath, String uri){
+        return String.join(CommonConstant.PATH_DELIMITER, nodePath, uri);
+    }
+}
