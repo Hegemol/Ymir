1d502eb4f02c7e272b96a688fd821657b5791bb8
KevinClair <704714211@qq.com>
2021-04-27 23:57:01 +0800

    调整本地缓存的Object对象类型，本地缓存只缓存对应Bean方法的某个实例，不缓存所有实例；

--- src/main/java/org/season/ymir/common/register/DefaultAbstractServiceRegister.java
+++ src/main/java/org/season/ymir/common/register/DefaultAbstractServiceRegister.java
@@ -6,7 +6,10 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.season.ymir.common.entity.ServiceBean;
+import org.season.ymir.common.entity.ServiceBeanCache;
 
+import java.net.InetAddress;
 import java.util.Objects;
 
 /**
@@ -24,32 +27,32 @@ public abstract class DefaultAbstractServiceRegister implements ServiceRegister 
      * 端口
      */
     protected Integer port;
-    /**
-     * 权重
-     */
-    protected Integer weight;
 
     // 本地缓存
-    private LoadingCache<String, ServiceBean> service = Caffeine.newBuilder()
+    private LoadingCache<String, ServiceBeanCache> service = Caffeine.newBuilder()
             .initialCapacity(10)
             .maximumSize(50000)
-            .build(new CacheLoader<String, ServiceBean>() {
+            .build(new CacheLoader<String, ServiceBeanCache>() {
                 @Nullable
                 @Override
-                public ServiceBean load(@NonNull String s) throws Exception {
+                public ServiceBeanCache load(@NonNull String s) throws Exception {
                     return null;
                 }
             });
 
     @Override
-    public ServiceBean getBean(final String name) throws Exception {
+    public ServiceBeanCache getBean(final String name) throws Exception {
         return service.get(name);
     }
 
     @Override
-    public void registerBean(final ServiceBean bean) throws Exception {
-        if (Objects.isNull(bean)) throw new IllegalArgumentException("parameter can not be empty.");
-        service.put(bean.getName(), bean);
+    public void registerBean(final ServiceBean serviceBean, final int weight) throws Exception {
+        if (Objects.isNull(serviceBean)) throw new IllegalArgumentException("parameter can not be empty.");
+        String host = InetAddress.getLocalHost().getHostAddress();
+        String address = host + ":" + port;
+        serviceBean.setAddress(address);
+        serviceBean.setProtocol(protocol);
+        service.put(serviceBean.getName(), new ServiceBeanCache(serviceBean.getName(), serviceBean.getClazz(), serviceBean.getBean()));
     }
 }
 
