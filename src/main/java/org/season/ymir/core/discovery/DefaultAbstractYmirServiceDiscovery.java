2f51895688c4d9c03b7d0bd0a2b85bf8463d9a0e
KevinClair <704714211@qq.com>
2021-04-28 23:59:27 +0800

    调整本地缓存的Provider列表；

--- src/main/java/org/season/ymir/core/discovery/DefaultAbstractYmirServiceDiscovery.java
+++ src/main/java/org/season/ymir/core/discovery/DefaultAbstractYmirServiceDiscovery.java
@@ -1,4 +1,4 @@
-package org.season.ymir.core.cache;
+package org.season.ymir.core.discovery;
 
 import com.github.benmanes.caffeine.cache.CacheLoader;
 import com.github.benmanes.caffeine.cache.Caffeine;
@@ -8,7 +8,6 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 import org.season.ymir.common.entity.ServiceBean;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,7 +15,7 @@ import java.util.List;
  *
  * @author KevinClair
  **/
-public class YmirServerDiscoveryCache {
+public abstract class DefaultAbstractYmirServiceDiscovery implements YmirServiceDiscovery {
 
     // 本地缓存
     private static final LoadingCache<String, List<ServiceBean>> SERVER_MAP = Caffeine.newBuilder()
@@ -30,47 +29,28 @@ public class YmirServerDiscoveryCache {
                 }
             });
 
-    /**
-     * 客户端注入的远程服务service class
-     */
-    public static final List<String> SERVICE_LIST = new ArrayList<>();
-
-    /**
-     * 清空缓存
-     *
-     * @param serviceName
-     */
-    public static void remove(String serviceName) {
+    @Override
+    public void remove(String serviceName) {
         SERVER_MAP.invalidate(serviceName);
     }
 
-    /**
-     * 本地缓存列表是否为空
-     *
-     * @param serviceName
-     * @return
-     */
-    public static boolean isEmpty(String serviceName) {
+    @Override
+    public boolean isEmpty(String serviceName) {
         return CollectionUtils.isEmpty(SERVER_MAP.get(serviceName));
     }
 
-    /**
-     * 获取缓存
-     *
-     * @param serviceName
-     * @return
-     */
-    public static List<ServiceBean> get(String serviceName) {
+    @Override
+    public List<ServiceBean> get(String serviceName) {
         return SERVER_MAP.get(serviceName);
     }
 
-    /**
-     * 设置缓存
-     *
-     * @param serviceName
-     * @param serviceList
-     */
-    public static void put(String serviceName, List<ServiceBean> serviceList) {
+    @Override
+    public void put(String serviceName, List<ServiceBean> serviceList) {
         SERVER_MAP.put(serviceName, serviceList);
+    }
+
+    @Override
+    public List<ServiceBean> findServiceList(String name) throws Exception {
+        return SERVER_MAP.get(name);
     }
 }
