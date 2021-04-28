da75a49b055d6bc108729f7a321e748b069a52a8
KevinClair <704714211@qq.com>
2021-04-28 23:59:03 +0800

    调整Zookeeper节点监听；添加TODO列表；

--- src/main/java/org/season/ymir/core/zookeeper/CuratorListenerImpl.java
+++ src/main/java/org/season/ymir/core/zookeeper/CuratorListenerImpl.java
@@ -1,9 +1,9 @@
 package org.season.ymir.core.zookeeper;
 
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.api.CuratorEvent;
-import org.apache.curator.framework.api.CuratorListener;
-import org.season.ymir.core.cache.YmirServerDiscoveryCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
+import org.season.ymir.core.discovery.YmirServiceDiscovery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,14 +12,25 @@ import org.slf4j.LoggerFactory;
  *
  * @author KevinClair
  **/
-public class CuratorListenerImpl implements CuratorListener {
+public class CuratorListenerImpl implements PathChildrenCacheListener {
 
     private static Logger logger = LoggerFactory.getLogger(CuratorListenerImpl.class);
 
+    private YmirServiceDiscovery serviceDiscovery;
+
+    public CuratorListenerImpl(YmirServiceDiscovery serviceDiscovery) {
+        this.serviceDiscovery = serviceDiscovery;
+    }
+
+//    @Override
+//    public void eventReceived(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
+//        logger.debug("Zookeeper node change, path:{}, childList:{}, node data:{}", curatorEvent.getPath(), curatorEvent.getChildren(), new String(curatorEvent.getData()));
+//        // 只要子节点有改动就清空缓存
+//        serviceDiscovery.remove(curatorEvent.getPath());
+//    }
+
     @Override
-    public void eventReceived(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
-        logger.debug("Zookeeper node change, path:{}, childList:{}, node data:{}", curatorEvent.getPath(), curatorEvent.getChildren(), new String(curatorEvent.getData()));
-        // 只要子节点有改动就清空缓存
-        YmirServerDiscoveryCache.remove(curatorEvent.getPath());
+    public void childEvent(CuratorFramework curatorFramework, PathChildrenCacheEvent pathChildrenCacheEvent) throws Exception {
+        // TODO 根据不同的节点变更类型，更新本地缓存和远程zookeeper节点数据
     }
 }
