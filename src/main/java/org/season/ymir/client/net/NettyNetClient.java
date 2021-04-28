861bc721d94cec531a429c2f3e46310672a0d213
KevinClair <704714211@qq.com>
2021-04-29 00:00:08 +0800

    Netty客户端逻辑；

--- src/main/java/org/season/ymir/client/net/NettyNetClient.java
+++ src/main/java/org/season/ymir/client/net/NettyNetClient.java
@@ -36,37 +36,6 @@ public class NettyNetClient {
      */
     public static Map<String, SendHandlerV2> connectedServerNodes = new ConcurrentHashMap<>();
 
-    public byte[] sendRequest(byte[] data, ServiceBean service) throws InterruptedException {
-        String address = service.getAddress();
-        String[] addrInfo = address.split(":");
-        final String serverAddress = addrInfo[0];
-        final String serverPort = addrInfo[1];
-        SendHandler sendHandler = new SendHandler(data);
-        byte[] respData;
-        // 配置客户端
-        EventLoopGroup loopGroup = new NioEventLoopGroup();
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(loopGroup).channel(NioSocketChannel.class)
-                    .option(ChannelOption.TCP_NODELAY, true)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel socketChannel) throws Exception {
-                            ChannelPipeline pipeline = socketChannel.pipeline();
-                            pipeline.addLast(sendHandler);
-                        }
-                    });
-            // 启用客户端连接
-            b.connect(serverAddress, Integer.parseInt(serverPort)).sync();
-            respData = (byte[]) sendHandler.respData();
-            logger.debug("SendRequest get reply: {}", respData);
-        } finally {
-            loopGroup.shutdownGracefully();
-        }
-
-        return respData;
-    }
-
     public YmirResponse sendRequest(YmirRequest rpcRequest, ServiceBean service, MessageProtocol messageProtocol) {
 
         String address = service.getAddress();
