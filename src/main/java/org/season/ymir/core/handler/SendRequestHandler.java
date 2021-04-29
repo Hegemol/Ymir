9b0444adba5f520d029b90c40faf81ada35961a1
KevinClair <704714211@qq.com>
2021-04-30 00:15:34 +0800

    请求发送处理器；

---
+++ src/main/java/org/season/ymir/core/handler/SendRequestHandler.java
@@ -0,0 +1,135 @@
+package org.season.ymir.core.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import org.season.ymir.client.net.NettyNetClient;
+import org.season.ymir.common.exception.RpcException;
+import org.season.ymir.common.model.YmirFuture;
+import org.season.ymir.common.model.YmirRequest;
+import org.season.ymir.common.model.YmirResponse;
+import org.season.ymir.core.protocol.MessageProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 发送请求处理器
+ *
+ * @author KevinClair
+ **/
+public class SendRequestHandler extends ChannelInboundHandlerAdapter {
+
+    private static Logger logger = LoggerFactory.getLogger(SendRequestHandler.class);
+
+    /**
+     * 编码器
+     */
+    private MessageProtocol messageProtocol;
+
+    /**
+     * 远程请求地址
+     */
+    private String remoteAddress;
+
+    /**
+     * 等待通道建立最大时间
+     */
+    static final int CHANNEL_WAIT_TIME = 4;
+    /**
+     * 等待响应最大时间
+     */
+    static final int RESPONSE_WAIT_TIME = 8;
+
+    /**
+     * 通道
+     */
+    private volatile Channel channel;
+
+    private static Map<String, YmirFuture<YmirResponse>> requestMap = new ConcurrentHashMap<>();
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public SendRequestHandler(MessageProtocol messageProtocol, String remoteAddress) {
+        this.messageProtocol = messageProtocol;
+        this.remoteAddress = remoteAddress;
+    }
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        this.channel = ctx.channel();
+        latch.countDown();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        logger.debug("Connect to server successfully:{}", ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        logger.debug("Client reads message:{}", msg);
+        ByteBuf byteBuf = (ByteBuf) msg;
+        byte[] resp = new byte[byteBuf.readableBytes()];
+        byteBuf.readBytes(resp);
+        // 手动回收
+        ReferenceCountUtil.release(byteBuf);
+        YmirResponse response = messageProtocol.unmarshallingResponse(resp);
+        YmirFuture<YmirResponse> future = requestMap.get(response.getRequestId());
+        future.setResponse(response);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        logger.error("Exception occurred:{}", cause.getMessage());
+        ctx.close();
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+        logger.error("channel inactive with remoteAddress:[{}]",remoteAddress);
+        NettyNetClient.connectedServerNodes.remove(remoteAddress);
+
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        super.userEventTriggered(ctx, evt);
+    }
+
+    public YmirResponse sendRequest(YmirRequest request) {
+        YmirResponse response;
+        YmirFuture<YmirResponse> future = new YmirFuture<>();
+        requestMap.put(request.getRequestId(), future);
+        try {
+            byte[] data = messageProtocol.marshallingRequest(request);
+            ByteBuf reqBuf = Unpooled.buffer(data.length);
+            reqBuf.writeBytes(data);
+            if (latch.await(CHANNEL_WAIT_TIME, TimeUnit.SECONDS)){
+                channel.writeAndFlush(reqBuf);
+                // 等待响应
+                response = future.get(RESPONSE_WAIT_TIME, TimeUnit.SECONDS);
+            }else {
+                throw new RpcException("establish channel time out");
+            }
+        } catch (Exception e) {
+            throw new RpcException(e.getMessage());
+        } finally {
+            requestMap.remove(request.getRequestId());
+        }
+        return response;
+    }
+}
