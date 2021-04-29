1f52f1b8f55a95abfb013ce7149199357f301a77
KevinClair <704714211@qq.com>
2021-04-30 00:16:06 +0800

    zookeeper节点监听；

--- src/main/java/org/season/ymir/core/zookeeper/CuratorListenerImpl.java
+++ src/main/java/org/season/ymir/core/zookeeper/CuratorListenerImpl.java
@@ -3,9 +3,12 @@ package org.season.ymir.core.zookeeper;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
+import org.season.ymir.common.constant.CommonConstant;
 import org.season.ymir.core.discovery.YmirServiceDiscovery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
 
 /**
  * zk节点监听
@@ -22,15 +25,22 @@ public class CuratorListenerImpl implements PathChildrenCacheListener {
         this.serviceDiscovery = serviceDiscovery;
     }
 
-//    @Override
-//    public void eventReceived(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
-//        logger.debug("Zookeeper node change, path:{}, childList:{}, node data:{}", curatorEvent.getPath(), curatorEvent.getChildren(), new String(curatorEvent.getData()));
-//        // 只要子节点有改动就清空缓存
-//        serviceDiscovery.remove(curatorEvent.getPath());
-//    }
-
     @Override
     public void childEvent(CuratorFramework curatorFramework, PathChildrenCacheEvent pathChildrenCacheEvent) throws Exception {
         // TODO 根据不同的节点变更类型，更新本地缓存和远程zookeeper节点数据
+        logger.debug("Ymir service listener change,event type:{}, path:{}, data:{}",pathChildrenCacheEvent.getType().name(), pathChildrenCacheEvent.getData().getPath(), new String(pathChildrenCacheEvent.getData().getData(), CommonConstant.UTF_8));
+        switch (pathChildrenCacheEvent.getType()){
+            case CHILD_ADDED:
+                serviceDiscovery.put(pathChildrenCacheEvent.getData().getPath(), Collections.EMPTY_LIST);
+                break;
+            case CHILD_REMOVED:
+                serviceDiscovery.remove(pathChildrenCacheEvent.getData().getPath());
+                break;
+            case CHILD_UPDATED:
+                serviceDiscovery.put(pathChildrenCacheEvent.getData().getPath(), Collections.EMPTY_LIST);
+                break;
+            default:
+                break;
+        }
     }
 }
