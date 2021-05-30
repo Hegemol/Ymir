dd0eda870bb34a47d179c90c3b341dfcde579fda
KevinClair <704714211@qq.com>
2021-05-30 23:33:45 +0800

    Add filter.

---
+++ src/main/java/org/season/ymir/core/filter/Filter.java
@@ -0,0 +1,9 @@
+package org.season.ymir.core.filter;
+
+/**
+ * 过滤器
+ *
+ * @author KevinClair
+ **/
+public interface Filter {
+}
---
+++ src/main/java/org/season/ymir/core/filter/FilterChain.java
@@ -0,0 +1,9 @@
+package org.season.ymir.core.filter;
+
+/**
+ * 过滤器执行器
+ *
+ * @author KevinClair
+ **/
+public class FilterChain {
+}
