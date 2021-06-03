a1f769fcde9f3a0de2289d7f5b37d2cd567fd736
KevinClair <704714211@qq.com>
2021-06-03 21:38:22 +0800

    添加过滤器；

--- src/main/java/org/season/ymir/core/filter/Filter.java
+++ src/main/java/org/season/ymir/core/filter/Filter.java
@@ -1,9 +1,19 @@
 package org.season.ymir.core.filter;
 
+import org.season.ymir.common.entity.ServiceBean;
+
 /**
  * 过滤器
  *
  * @author KevinClair
  **/
+//@SPI()
 public interface Filter {
+
+    /**
+     * 处理器
+     *
+     * @param serviceBean {@link ServiceBean}
+     */
+    void handler(ServiceBean serviceBean);
 }
