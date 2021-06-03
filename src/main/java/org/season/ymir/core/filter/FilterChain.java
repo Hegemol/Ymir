866a95dc16d003befc208b0c04e9a083b7a8296a
KevinClair <704714211@qq.com>
2021-06-03 21:37:35 +0800

    添加执行器；

--- src/main/java/org/season/ymir/core/filter/FilterChain.java
+++ src/main/java/org/season/ymir/core/filter/FilterChain.java
@@ -1,9 +1,20 @@
 package org.season.ymir.core.filter;
 
+import org.season.ymir.common.entity.ServiceBean;
+import org.season.ymir.common.model.YmirResponse;
+
 /**
  * 过滤器执行器
  *
  * @author KevinClair
  **/
-public class FilterChain {
+public interface FilterChain {
+
+    /**
+     * 执行器
+     *
+     * @param bean
+     * @return
+     */
+    YmirResponse execute(ServiceBean bean);
 }
