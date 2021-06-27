62a0d3ecd8fc578fc1ff4c17af4fd56e67fcbb94
KevinClair <704714211@qq.com>
2021-06-27 21:38:44 +0800

    Add gson protocol.

---
+++ src/main/java/org/season/ymir/core/protocol/GsonMessageProtocol.java
@@ -0,0 +1,36 @@
+package org.season.ymir.core.protocol;
+
+import org.season.ymir.common.model.YmirRequest;
+import org.season.ymir.common.model.YmirResponse;
+import org.season.ymir.common.utils.GsonUtils;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Gson序列化
+ *
+ * @author KevinClair
+ **/
+public class GsonMessageProtocol implements MessageProtocol {
+
+    @Override
+    public byte[] marshallingRequest(YmirRequest request) throws Exception {
+        return GsonUtils.getInstance().toJson(request).getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public YmirRequest unmarshallingRequest(byte[] data) throws Exception {
+        return GsonUtils.getInstance().fromJson(new String(data, Charset.forName("utf-8")), YmirRequest.class);
+    }
+
+    @Override
+    public byte[] marshallingResponse(YmirResponse response) throws Exception {
+        return GsonUtils.getInstance().toJson(response).getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public YmirResponse unmarshallingResponse(byte[] data) throws Exception {
+        return GsonUtils.getInstance().fromJson(new String(data, Charset.forName("utf-8")), YmirResponse.class);
+    }
+}
