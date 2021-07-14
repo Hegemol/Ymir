946bfe40cc656f671213b1029e79e2c559ddc0a6
KevinClair <704714211@qq.com>
2021-07-14 22:11:37 +0800

    Add encoder handler.

--- src/main/java/org/season/ymir/core/codec/MessageRequestEncoder.java
+++ src/main/java/org/season/ymir/core/codec/MessageRequestEncoder.java
@@ -4,6 +4,11 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import org.season.ymir.common.model.YmirRequest;
+import org.season.ymir.common.utils.GsonUtils;
+import org.season.ymir.core.protocol.MessageProtocol;
+import org.season.ymir.spi.loader.ExtensionLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 消息请求编码器
@@ -12,8 +17,22 @@ import org.season.ymir.common.model.YmirRequest;
  **/
 public class MessageRequestEncoder extends MessageToByteEncoder<YmirRequest> {
 
+    private static final Logger logger = LoggerFactory.getLogger(MessageRequestEncoder.class);
+
+    private String protocol;
+
+    public MessageRequestEncoder(String protocol) {
+        this.protocol = protocol;
+    }
+
     @Override
     protected void encode(ChannelHandlerContext channelHandlerContext, YmirRequest ymirRequest, ByteBuf byteBuf) throws Exception {
-
+        MessageProtocol messageProtocol = ExtensionLoader.getExtensionLoader(MessageProtocol.class).getLoader(protocol);
+        byte[] request = messageProtocol.marshallingRequest(ymirRequest);
+        byteBuf.writeInt(request.length);
+        byteBuf.writeBytes(request);
+        if (logger.isDebugEnabled()){
+            logger.debug("Channel {} encoder request message success, message content:{}", channelHandlerContext.channel().id(), GsonUtils.getInstance().toJson(ymirRequest));
+        }
     }
 }
--- src/main/java/org/season/ymir/core/codec/MessageResponseEncoder.java
+++ src/main/java/org/season/ymir/core/codec/MessageResponseEncoder.java
@@ -4,6 +4,11 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import org.season.ymir.common.model.YmirResponse;
+import org.season.ymir.common.utils.GsonUtils;
+import org.season.ymir.core.protocol.MessageProtocol;
+import org.season.ymir.spi.loader.ExtensionLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 消息相应编码器
@@ -11,8 +16,23 @@ import org.season.ymir.common.model.YmirResponse;
  * @author KevinClair
  **/
 public class MessageResponseEncoder extends MessageToByteEncoder<YmirResponse> {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageResponseEncoder.class);
+
+    private String protocol;
+
+    public MessageResponseEncoder(String protocol) {
+        this.protocol = protocol;
+    }
+
     @Override
     protected void encode(ChannelHandlerContext channelHandlerContext, YmirResponse ymirResponse, ByteBuf byteBuf) throws Exception {
-
+        MessageProtocol messageProtocol = ExtensionLoader.getExtensionLoader(MessageProtocol.class).getLoader(protocol);
+        byte[] request = messageProtocol.marshallingResponse(ymirResponse);
+        byteBuf.writeInt(request.length);
+        byteBuf.writeBytes(request);
+        if (logger.isDebugEnabled()){
+            logger.debug("Channel {} encoder response message success, message content:{}", channelHandlerContext.channel().id(), GsonUtils.getInstance().toJson(ymirResponse));
+        }
     }
 }
