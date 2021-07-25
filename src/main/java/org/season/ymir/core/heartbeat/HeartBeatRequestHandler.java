c3d1a938d893862d0881216d452d64fab24a5c4d
KevinClair <704714211@qq.com>
2021-07-25 22:54:46 +0800

    Add heart beat handler.

---
+++ src/main/java/org/season/ymir/core/heartbeat/HeartBeatRequestHandler.java
@@ -0,0 +1,9 @@
+package org.season.ymir.core.heartbeat;
+
+/**
+ * 心跳请求处理器
+ *
+ * @author KevinClair
+ **/
+public class HeartBeatRequestHandler {
+}
---
+++ src/main/java/org/season/ymir/core/heartbeat/HeartBeatResponseHandler.java
@@ -0,0 +1,9 @@
+package org.season.ymir.core.heartbeat;
+
+/**
+ * 心跳响应处理器
+ *
+ * @author KevinClair
+ **/
+public class HeartBeatResponseHandler {
+}
