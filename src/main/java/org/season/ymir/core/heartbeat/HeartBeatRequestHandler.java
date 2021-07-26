17915611561a50b226fd7196d747a7eb4a520ffa
KevinClair <704714211@qq.com>
2021-07-26 12:17:35 +0800

    Add heart beat handler.

--- src/main/java/org/season/ymir/core/heartbeat/HeartBeatRequestHandler.java
+++ src/main/java/org/season/ymir/core/heartbeat/HeartBeatRequestHandler.java
@@ -1,9 +1,40 @@
 package org.season.ymir.core.heartbeat;
 
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import org.season.ymir.common.base.ServiceStatusEnum;
+import org.season.ymir.common.constant.CommonConstant;
+import org.season.ymir.common.model.YmirRequest;
+import org.season.ymir.common.model.YmirResponse;
+import org.season.ymir.common.utils.GsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 心跳请求处理器
  *
  * @author KevinClair
  **/
-public class HeartBeatRequestHandler {
+@ChannelHandler.Sharable
+public class HeartBeatRequestHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(HeartBeatRequestHandler.class);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        YmirRequest request = (YmirRequest) msg;
+        if (request.getRequestId().equals(CommonConstant.HEART_BEAT_REQUEST)){
+            if (logger.isDebugEnabled()){
+            }
+            logger.info("Server heart beat request:{}", GsonUtils.getInstance().toJson(request));
+            YmirResponse response = new YmirResponse(ServiceStatusEnum.SUCCESS);
+            response.setRequestId(CommonConstant.HEART_BEAT_RESPONSE);
+            ctx.channel().writeAndFlush(response);
+            ReferenceCountUtil.release(msg);
+        } else {
+            ctx.fireChannelRead(msg);
+        }
+    }
 }
--- src/main/java/org/season/ymir/core/heartbeat/HeartBeatResponseHandler.java
+++ src/main/java/org/season/ymir/core/heartbeat/HeartBeatResponseHandler.java
@@ -1,9 +1,36 @@
 package org.season.ymir.core.heartbeat;
 
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import org.season.ymir.common.constant.CommonConstant;
+import org.season.ymir.common.model.YmirResponse;
+import org.season.ymir.common.utils.GsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 心跳响应处理器
  *
  * @author KevinClair
  **/
-public class HeartBeatResponseHandler {
+@ChannelHandler.Sharable
+public class HeartBeatResponseHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(HeartBeatResponseHandler.class);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // 心跳回应
+        YmirResponse response = (YmirResponse) msg;
+        if (response.getRequestId().equals(CommonConstant.HEART_BEAT_RESPONSE)){
+            if (logger.isDebugEnabled()){
+            }
+            logger.info("Client receive heart beat response:{}", GsonUtils.getInstance().toJson(msg));
+            ReferenceCountUtil.release(msg);
+        } else {
+            ctx.fireChannelRead(msg);
+        }
+    }
 }
