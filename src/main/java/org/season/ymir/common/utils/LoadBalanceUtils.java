43dfd8b4b201d6bf5a86949456ab27a2e626ca72
KevinClair <704714211@qq.com>
2021-07-28 00:47:28 +0800

    Update load balance util's method, use url to match ServiceBean.

--- src/main/java/org/season/ymir/common/utils/LoadBalanceUtils.java
+++ src/main/java/org/season/ymir/common/utils/LoadBalanceUtils.java
@@ -1,6 +1,8 @@
 package org.season.ymir.common.utils;
 
+import org.apache.commons.lang3.StringUtils;
 import org.season.ymir.common.entity.ServiceBean;
+import org.season.ymir.common.exception.RpcException;
 import org.season.ymir.core.balance.LoadBalance;
 import org.season.ymir.spi.loader.ExtensionLoader;
 
@@ -18,11 +20,15 @@ public class LoadBalanceUtils {
      *
      * @param serviceBeans 服务Bean集合
      * @param type         负载均衡类型
-     * @param ip           服务ip地址
+     * @param url          服务直连url
+     * @param address      服务地址
      * @return {@link ServiceBean}
      */
-    public static ServiceBean selector(final List<ServiceBean> serviceBeans, final String type, final String ip) {
+    public static ServiceBean selector(final List<ServiceBean> serviceBeans, final String type, final String url, final String address) {
+        if (StringUtils.isNotBlank(url)){
+            return serviceBeans.stream().filter(each -> each.getAddress().equals(url)).findFirst().orElseThrow(() -> new RpcException(String.format("No provider available for service %s from url %s", serviceBeans.get(0).getName(), url)));
+        }
         LoadBalance loadBalance = ExtensionLoader.getExtensionLoader(LoadBalance.class).getLoader(type);
-        return loadBalance.load(serviceBeans, ip);
+        return loadBalance.load(serviceBeans, address);
     }
 }
