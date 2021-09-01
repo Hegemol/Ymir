7f4fc3fbcbb261e1e73e625852db7cfc262b994c
KevinClair <704714211@qq.com>
2021-09-01 23:56:31 +0800

    Fix bug, add kryo serialization.

---
+++ src/main/java/org/season/ymir/core/protocol/kryo/KryoMessageProtocol.java
@@ -0,0 +1,60 @@
+package org.season.ymir.core.protocol.kryo;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.pool.KryoPool;
+import org.season.ymir.common.model.InvocationMessage;
+import org.season.ymir.core.protocol.MessageProtocol;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class KryoMessageProtocol implements MessageProtocol {
+
+    private KryoPool pool = KryoPoolFactory.getKryoPoolInstance();
+
+    @Override
+    public byte[] marshalling(final Object object) throws Exception {
+        Kryo kryo = pool.borrow();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Output out = new Output(byteArrayOutputStream);
+        try {
+            kryo.writeObject(out, object);
+            out.close();
+            return byteArrayOutputStream.toByteArray();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                byteArrayOutputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            pool.release(kryo);
+        }
+    }
+
+    @Override
+    public InvocationMessage unmarshalling(final byte[] data) throws Exception {
+        Kryo kryo = pool.borrow();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+        Input in = new Input(byteArrayInputStream);
+        try {
+            InvocationMessage result = kryo.readObject(in, InvocationMessage.class);
+            in.close();
+            return result;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                byteArrayInputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            pool.release(kryo);
+        }
+    }
+
+}
---
+++ src/main/java/org/season/ymir/core/protocol/kryo/KryoPoolFactory.java
@@ -0,0 +1,43 @@
+package org.season.ymir.core.protocol.kryo;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.pool.KryoFactory;
+import com.esotericsoftware.kryo.pool.KryoPool;
+import org.objenesis.strategy.StdInstantiatorStrategy;
+import org.season.ymir.common.model.InvocationMessage;
+
+public class KryoPoolFactory {
+    private static volatile KryoPoolFactory poolFactory = null;
+
+    private KryoFactory factory = new KryoFactory() {
+        @Override
+        public Kryo create() {
+            Kryo kryo = new Kryo();
+            kryo.setReferences(false);
+            kryo.register(InvocationMessage.class);
+            Kryo.DefaultInstantiatorStrategy strategy = (Kryo.DefaultInstantiatorStrategy) kryo.getInstantiatorStrategy();
+            strategy.setFallbackInstantiatorStrategy(new StdInstantiatorStrategy());
+            return kryo;
+        }
+    };
+
+    private KryoPool pool = new KryoPool.Builder(factory).build();
+
+    private KryoPoolFactory() {
+    }
+
+    public static KryoPool getKryoPoolInstance() {
+        if (poolFactory == null) {
+            synchronized (KryoPoolFactory.class) {
+                if (poolFactory == null) {
+                    poolFactory = new KryoPoolFactory();
+                }
+            }
+        }
+        return poolFactory.getPool();
+    }
+
+    public KryoPool getPool() {
+        return pool;
+    }
+}
