a5ef4585203ff6f9a6855e294c07f08b2da25463
KevinClair <704714211@qq.com>
2021-09-05 18:18:23 +0800

    Add scheme cache.

--- src/main/java/org/season/ymir/core/protocol/ProtostuffMessageProtocol.java
+++ src/main/java/org/season/ymir/core/protocol/ProtostuffMessageProtocol.java
@@ -5,7 +5,12 @@ import com.dyuproject.protostuff.LinkedBuffer;
 import com.dyuproject.protostuff.ProtostuffIOUtil;
 import com.dyuproject.protostuff.Schema;
 import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
 import org.season.ymir.common.model.InvocationMessage;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 消息协议
@@ -14,14 +19,24 @@ import org.season.ymir.common.model.InvocationMessage;
  */
 public class ProtostuffMessageProtocol implements MessageProtocol{
 
+    // Schema缓存
+    private Map<Class<?>, Schema<?>> cachedSchema = new ConcurrentHashMap<>();
+
+    private Objenesis objenesis = new ObjenesisStd(true);
+
     @Override
-    public byte[] marshalling(Object object) throws Exception {
-        return serialize(object);
+    public byte[] serialize(Object object) throws Exception {
+        return serializeExecute(object);
     }
 
     @Override
-    public InvocationMessage unmarshalling(final byte[] data) throws Exception {
-        return deserialize(data,InvocationMessage.class);
+    public InvocationMessage deserialize(final byte[] data) throws Exception {
+        return deserializeExecute(data,InvocationMessage.class);
+    }
+
+    private <T> Schema<T> getSchema(Class<T> cls) {
+        // for thread-safe
+        return (Schema<T>) cachedSchema.computeIfAbsent(cls, RuntimeSchema::createFrom);
     }
 
     /**
@@ -31,11 +46,14 @@ public class ProtostuffMessageProtocol implements MessageProtocol{
      * @param <T>    泛型类型
      * @return byte[]
      */
-    private <T> byte[] serialize(T source) {
-        Schema<T> schema = RuntimeSchema.getSchema((Class<T>) source.getClass());
+    private <T> byte[] serializeExecute(T source) {
+        Class<T> cls = (Class<T>) source.getClass();
         LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
         try {
+            Schema<T> schema = getSchema(cls);
             return ProtostuffIOUtil.toByteArray(source, schema, buffer);
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
         } finally {
             buffer.clear();
         }
@@ -49,10 +67,14 @@ public class ProtostuffMessageProtocol implements MessageProtocol{
      * @param <T>    泛型类型
      * @return 反序列化后的对象
      */
-    private <T> T deserialize(byte[] source, Class<T> clazz) {
-        Schema<T> schema = RuntimeSchema.getSchema(clazz);
-        T t = schema.newMessage();
-        ProtostuffIOUtil.mergeFrom(source, t, schema);
-        return t;
+    private <T> T deserializeExecute(byte[] source, Class<T> clazz) {
+        try {
+            T message = objenesis.newInstance(clazz);
+            Schema<T> schema = getSchema(clazz);
+            ProtostuffIOUtil.mergeFrom(source, message, schema);
+            return message;
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
     }
 }
