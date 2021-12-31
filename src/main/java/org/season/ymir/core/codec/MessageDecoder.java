d8870e44bc4a95a38632f6d3c93736c2ba47d376
KevinClair <kevinclair@apache.org>
2021-12-31 17:06:47 +0800

    Netty decoder refactor.

--- src/main/java/org/season/ymir/core/codec/MessageDecoder.java
+++ src/main/java/org/season/ymir/core/codec/MessageDecoder.java
@@ -2,51 +2,49 @@ package org.season.ymir.core.codec;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
-import org.season.ymir.common.model.InvocationMessage;
-import org.season.ymir.common.utils.GsonUtils;
-import org.season.ymir.core.protocol.MessageProtocol;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.List;
 
 /**
  * 消息解码器
  *
  * @author KevinClair
  **/
-public class MessageDecoder extends ByteToMessageDecoder {
+public class MessageDecoder extends LengthFieldBasedFrameDecoder {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageDecoder.class);
 
-    private MessageProtocol protocol;
-
-    public MessageDecoder(MessageProtocol protocol) {
-        this.protocol = protocol;
+    public MessageDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength, int lengthAdjustment, int initialBytesToStrip) {
+        super(maxFrameLength, lengthFieldOffset, lengthFieldLength, lengthAdjustment, initialBytesToStrip);
     }
 
     @Override
-    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
-        // 标记当前读取位置
-        byteBuf.markReaderIndex();
-        // 判断是否能够读取length长度
-        if (byteBuf.readableBytes() < 4){
-            return;
-        }
-        // 读取长度
-        int bytesSize = byteBuf.readInt();
-        // 如果 message 不够可读，则退回到原读取位置
-        if (byteBuf.readableBytes() < bytesSize) {
-            byteBuf.resetReaderIndex();
-            return;
-        }
-        byte[] bytes = new byte[bytesSize];
-        byteBuf.readBytes(bytes);
-        InvocationMessage decodeResponse = protocol.deserialize(bytes);
-        list.add(decodeResponse);
-        if (logger.isDebugEnabled()){
-            logger.debug("Channel {} decoder message success, message content:{}", channelHandlerContext.channel().id(), GsonUtils.getInstance().toJson(decodeResponse));
-        }
+    protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+        return super.decode(ctx, in);
     }
+
+    //@Override
+    //protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+    //    // 标记当前读取位置
+    //    byteBuf.markReaderIndex();
+    //    // 判断是否能够读取length长度
+    //    if (byteBuf.readableBytes() < 4){
+    //        return;
+    //    }
+    //    // 读取长度
+    //    int bytesSize = byteBuf.readInt();
+    //    // 如果 message 不够可读，则退回到原读取位置
+    //    if (byteBuf.readableBytes() < bytesSize) {
+    //        byteBuf.resetReaderIndex();
+    //        return;
+    //    }
+    //    byte[] bytes = new byte[bytesSize];
+    //    byteBuf.readBytes(bytes);
+    //    InvocationMessage decodeResponse = protocol.deserialize(bytes);
+    //    list.add(decodeResponse);
+    //    if (logger.isDebugEnabled()){
+    //        logger.debug("Channel {} decoder message success, message content:{}", channelHandlerContext.channel().id(), GsonUtils.getInstance().toJson(decodeResponse));
+    //    }
+    //}
 }
