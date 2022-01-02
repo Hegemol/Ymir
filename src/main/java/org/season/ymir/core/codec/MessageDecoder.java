4bb8568f518b1efbc7c168ad8bab9d82ed855bcf
KevinClair <704714211@qq.com>
2022-01-02 20:21:30 +0800

    Message encoder and decoder refactor.

--- src/main/java/org/season/ymir/core/codec/MessageDecoder.java
+++ src/main/java/org/season/ymir/core/codec/MessageDecoder.java
@@ -3,14 +3,38 @@ package org.season.ymir.core.codec;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.season.ymir.common.base.MessageTypeEnum;
+import org.season.ymir.common.base.SerializationTypeEnum;
+import org.season.ymir.common.constant.CommonConstant;
+import org.season.ymir.common.model.InvocationMessage;
+import org.season.ymir.common.model.InvocationMessageWrap;
+import org.season.ymir.common.utils.GsonUtils;
+import org.season.ymir.core.protocol.MessageProtocol;
+import org.season.ymir.spi.loader.ExtensionLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 /**
- * 消息解码器
+ * 自定义协议
+ *
+ *   0     1     2     3     4     5     6     7     8     9     10     11    12    13    14
+ *   +-----+-----+-----+-----+----—+-----+-----+-----+-----+------+-----+-----+-----+-----+
+ *   |   magic   code        |      requestId        | type|serial|       full length     |
+ *   +-----------------------+-----------------------+-----+------+-----------------------+
+ *   |                                                                                    |
+ *   |                                       body                                         |
+ *   |                                                                                    |
+ *   |                                                                                    |
+ *   +------------------------------------------------------------------------------------+
+ * 4B  magic code（魔法数）   4B requestId（请求的Id）    1B type（消息类型）
+ * 1B serial（序列化类型）    4B  full length（消息长度）
+ * body（object类型数据）
  *
  * @author KevinClair
- **/
+ */
 public class MessageDecoder extends LengthFieldBasedFrameDecoder {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageDecoder.class);
@@ -21,30 +45,60 @@ public class MessageDecoder extends LengthFieldBasedFrameDecoder {
 
     @Override
     protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
-        return super.decode(ctx, in);
+        Object decode = super.decode(ctx, in);
+        if (decode instanceof ByteBuf){
+            ByteBuf byteBuf = (ByteBuf) decode;
+            // 判断可读长度
+            if (byteBuf.readableBytes() >= CommonConstant.TOTAL_LENGTH){
+                try {
+                    return decode(byteBuf);
+                } catch (Exception e) {
+                    logger.error("Decode message error:{}", ExceptionUtils.getStackTrace(e));
+                } finally {
+                    // 释放
+                    byteBuf.release();
+                }
+            }
+        }
+        return decode;
     }
 
-    //@Override
-    //protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
-    //    // 标记当前读取位置
-    //    byteBuf.markReaderIndex();
-    //    // 判断是否能够读取length长度
-    //    if (byteBuf.readableBytes() < 4){
-    //        return;
-    //    }
-    //    // 读取长度
-    //    int bytesSize = byteBuf.readInt();
-    //    // 如果 message 不够可读，则退回到原读取位置
-    //    if (byteBuf.readableBytes() < bytesSize) {
-    //        byteBuf.resetReaderIndex();
-    //        return;
-    //    }
-    //    byte[] bytes = new byte[bytesSize];
-    //    byteBuf.readBytes(bytes);
-    //    InvocationMessage decodeResponse = protocol.deserialize(bytes);
-    //    list.add(decodeResponse);
-    //    if (logger.isDebugEnabled()){
-    //        logger.debug("Channel {} decoder message success, message content:{}", channelHandlerContext.channel().id(), GsonUtils.getInstance().toJson(decodeResponse));
-    //    }
-    //}
+    private Object decode(ByteBuf byteBuf) throws Exception {
+        // 校验魔法值是否正确
+        checkMagicNumber(byteBuf);
+        // 读取requestId
+        int requestId = byteBuf.readInt();
+        // 读取消息类型
+        byte type = byteBuf.readByte();
+        // 序列化类型
+        byte serialType = byteBuf.readByte();
+        // 消息长度
+        int messageLength = byteBuf.readInt();
+        byte[] body = new byte[messageLength];
+        byteBuf.readBytes(body);
+        SerializationTypeEnum serializationType = SerializationTypeEnum.getType(serialType);
+        InvocationMessage message = ExtensionLoader.getExtensionLoader(MessageProtocol.class).getLoader(serializationType.getName()).deserialize(body);
+        InvocationMessageWrap messageWrap = new InvocationMessageWrap();
+        messageWrap.setRequestId(requestId);
+        messageWrap.setType(MessageTypeEnum.getType(type));
+        messageWrap.setSerial(serializationType);
+        messageWrap.setData(message);
+
+        if (logger.isDebugEnabled()){
+            logger.debug("Channel {} decoder message success, message content:{}", GsonUtils.getInstance().toJson(message));
+        }
+        return messageWrap;
+    }
+
+    private void checkMagicNumber(ByteBuf byteBuf) {
+        // 读取魔法值
+        int magicNumberLength = CommonConstant.MAGIC_NUMBER.length;
+        byte[] magicNums = new byte[magicNumberLength];
+        byteBuf.readBytes(magicNums);
+        for (int i = 0; i < magicNumberLength; i++) {
+            if (magicNums[i] != CommonConstant.MAGIC_NUMBER[i]) {
+                throw new IllegalArgumentException(String.format("Invalid magic code: %s", Arrays.toString(magicNums)));
+            }
+        }
+    }
 }
--- src/main/java/org/season/ymir/core/codec/MessageEncoder.java
+++ src/main/java/org/season/ymir/core/codec/MessageEncoder.java
@@ -3,42 +3,54 @@ package org.season.ymir.core.codec;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import org.season.ymir.common.base.SerializationTypeEnum;
+import org.season.ymir.common.constant.CommonConstant;
+import org.season.ymir.common.model.InvocationMessageWrap;
 import org.season.ymir.common.utils.GsonUtils;
 import org.season.ymir.core.protocol.MessageProtocol;
+import org.season.ymir.spi.loader.ExtensionLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 消息相应编码器
+ * 消息编码器，按照协议类型写入数据
  *
- *   0     1     2     3     4
- *   +-----+-----+-----+-----+
- *   |      full length      |
- *   +-----------------------+
- *   |                       |
- *   |        body           |
- *   |                       |
- *   |      ... ...          |
- *   +-----------------------+
- * 4B full length（消息长度，不是消息总长度） body（object类型数据）
+ *   0     1     2     3     4     5     6     7     8     9     10     11    12    13    14
+ *   +-----+-----+-----+-----+----—+-----+-----+-----+-----+------+-----+-----+-----+-----+
+ *   |   magic   code        |      requestId        | type|serial|       full length     |
+ *   +-----------------------+-----------------------+-----+------+-----------------------+
+ *   |                                                                                    |
+ *   |                                       body                                         |
+ *   |                                                                                    |
+ *   |                                                                                    |
+ *   +------------------------------------------------------------------------------------+
+ * 4B  magic code（魔法数）   4B requestId（请求的Id）    1B type（消息类型）
+ * 1B serial（序列化类型）    4B  full length（消息长度）
+ * body（object类型数据）
  *
  * @author KevinClair
- **/
-public class MessageEncoder extends MessageToByteEncoder<Object> {
+ */
+public class MessageEncoder extends MessageToByteEncoder<InvocationMessageWrap> {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageEncoder.class);
-
-    private MessageProtocol protocol;
-
-    public MessageEncoder(final MessageProtocol protocol)    {
-        this.protocol = protocol;
-    }
-
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, Object object, ByteBuf byteBuf) throws Exception {
-        byte[] request = protocol.serialize(object);
-        byteBuf.writeInt(request.length);
-        byteBuf.writeBytes(request);
+    protected void encode(ChannelHandlerContext channelHandlerContext, InvocationMessageWrap object, ByteBuf byteBuf) throws Exception {
+        // 魔法值
+        byteBuf.writeBytes(CommonConstant.MAGIC_NUMBER);
+        // 请求Id
+        byteBuf.writeInt(object.getRequestId());
+        // 消息类型
+        byteBuf.writeByte(object.getType().getCode());
+        // 序列化类型
+        SerializationTypeEnum serial = object.getSerial();
+        byteBuf.writeByte(serial.getCode());
+        // 消息长度
+        MessageProtocol protocol = ExtensionLoader.getExtensionLoader(MessageProtocol.class).getLoader(serial.getName());
+        byte[] body = protocol.serialize(object.getData());
+        byteBuf.writeInt(body.length);
+
+        // 写入最终的消息
+        byteBuf.writeBytes(body);
         if (logger.isDebugEnabled()){
             logger.debug("Channel {} encoder message success, message content:{}", channelHandlerContext.channel().id(), GsonUtils.getInstance().toJson(object));
         }
