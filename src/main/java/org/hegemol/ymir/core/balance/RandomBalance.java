546df1d8f37e39a1fecba455bfdc26c468037313
KevinClair <704714211@qq.com>
2022-09-18 23:31:23 +0800

    随机算法优化

--- src/main/java/org/hegemol/ymir/core/balance/RandomBalance.java
+++ src/main/java/org/hegemol/ymir/core/balance/RandomBalance.java
@@ -1,11 +1,9 @@
 package org.hegemol.ymir.core.balance;
 
-import org.hegemol.ymir.common.entity.ServiceBean;
-
-import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
+
+import org.hegemol.ymir.common.entity.ServiceBean;
 
 /**
  * 随机算法
@@ -16,35 +14,48 @@ public class RandomBalance extends AbstractLoadBalance {
 
     @Override
     public ServiceBean loadMethod(List<ServiceBean> services, String address) {
-        // 计算总权重
-        int totalWeight = calculateTotalWeight(services);
-        // 判断是否是相同的权重值
-        boolean sameWeight = isAllUpStreamSameWeight(services);
-
-        if (totalWeight > 0 && !sameWeight) {
-            return load(totalWeight, services);
-        }
-        return services.get(random.nextInt(services.size()));
-    }
-
-    private boolean isAllUpStreamSameWeight(List<ServiceBean> services) {
-        return new HashSet<>(services.stream().map(each -> each.getWeight()).collect(Collectors.toList())).size() == 1;
-    }
-
-    private int calculateTotalWeight(List<ServiceBean> services) {
-        return services.stream().mapToInt(each -> each.getWeight()).sum();
-    }
-
-    private ServiceBean load(final int totalWeight, final List<ServiceBean> services) {
-        // If the weights are not the same and the weights are greater than 0, then random by the total number of weights
-        int offset = random.nextInt(totalWeight);
-        // Determine which segment the random value falls on
-        for (ServiceBean serviceBean : services) {
-            offset -= serviceBean.getWeight();
-            if (offset < 0) {
-                return serviceBean;
+        int length = services.size();
+        // 标识符，用于识别是否权重相同
+        boolean sameWeight = true;
+        // 记录每个实例的权重
+        int[] weights = new int[length];
+        int firstUpstreamWeight = getWeight(services.get(0));
+        weights[0] = firstUpstreamWeight;
+        // 记录总权重
+        int totalWeight = firstUpstreamWeight;
+        for (int i = 1; i < length; i++) {
+            int currentUpstreamWeight = getWeight(services.get(i));
+            weights[i] = currentUpstreamWeight;
+            totalWeight += currentUpstreamWeight;
+            if (sameWeight && currentUpstreamWeight != firstUpstreamWeight) {
+                // 当前实例与第一个权重值不相同时
+                sameWeight = false;
             }
         }
-        return services.get(0);
+        if (totalWeight > 0 && !sameWeight) {
+            return random(totalWeight, weights, services);
+        }
+        return random(services);
+    }
+
+    private int getWeight(ServiceBean serviceBean){
+        return serviceBean.getWeight();
+    }
+
+    private ServiceBean random(final int totalWeight, final int[] weights, final List<ServiceBean> services) {
+        // 总权重中取一个随机数
+        int offset = random.nextInt(totalWeight);
+        // 计算当前的随机数在总权重的哪个位置
+        for (int i = 0; i < weights.length; i++) {
+            offset -= weights[i];
+            if (offset < 0) {
+                return services.get(i);
+            }
+        }
+        return random(services);
+    }
+
+    private ServiceBean random(final List<ServiceBean> services) {
+        return services.get(random.nextInt(services.size()));
     }
 }
